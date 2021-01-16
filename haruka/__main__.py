956b51a09aaff85f8a4f54deb60a5dfd49a88b36
Team Dark <75434157+sbtechz@users.noreply.github.com>
2021-01-16 17:42:04 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -22,24 +22,15 @@ from haruka.modules.connection import connected
 
 PM_START = """Hello {}, my name is {}!
 
-You know how hard it is sometimes to manage group so here is the solution for you
+ I'm here to help you manage your groups!
+ 
+ made with love by @hackerconsole
+ 
+ Type /help or click help button to find out more about me.
 
-I'm group manager bot and Anti-spam for RR Players🃏
+Join [our group for updates](t.me/hackerconsolechat)
 
-Activate Anti-spam protection in your BM by this command : 
-/antispam on 
-
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
-
-Special thanks to [this person](t.me/denver02) for helping me.
-
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
-
-Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
-
-Click /help or Help button below to find out more about how to use me to my full potential.
-
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+Add me to super group by clicking [here](http://t.me/groupmanagmentbot?startgroup=true).
 """
 
 
@@ -157,9 +148,9 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START
 
-    keyboard = [[InlineKeyboardButton(text="🇮🇳 Language", callback_data="set_lang_")]]
-    keyboard += [[InlineKeyboardButton(text="🛠 Reporting", callback_data="cntrl_panel_M"), 
-        InlineKeyboardButton(text="❔ Help", callback_data="help_back")]]
+    keyboard = [[InlineKeyboardButton(text="Language", callback_data="set_lang_")]]
+    keyboard += [[InlineKeyboardButton(text=" Reporting", callback_data="cntrl_panel_M"), 
+        InlineKeyboardButton(text=" Help", callback_data="help_back")]]
 
     update.effective_message.reply_text(PM_START.format(escape_markdown(first_name), bot.first_name), reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
 
@@ -195,7 +186,7 @@ def control_panel(bot, update):
     if M_match:
         text = "*Control panel* 🛠"
 
-        keyboard = [[InlineKeyboardButton(text="👤 My settings", callback_data="cntrl_panel_U(1)")]]
+        keyboard = [[InlineKeyboardButton(text=" My settings", callback_data="cntrl_panel_U(1)")]]
 
         #Show connected chat and add chat settings button
         conn = connected(bot, update, chat, user.id, need_admin=False)
